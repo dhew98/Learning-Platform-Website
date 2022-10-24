c1b0b80c1c80b1a9de0ef45d41a2cd3c47a4569d
Jawad Dhew <jawaddhew98@gmail.com>
2022-10-25 04:29:20 +0600

    email use

--- src/Pages/Header/Header.js
+++ src/Pages/Header/Header.js
@@ -1,12 +1,21 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import Container from 'react-bootstrap/Container';
+import Button from 'react-bootstrap/Button';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faUser, faGraduationCap } from '@fortawesome/free-solid-svg-icons';
 import { Link } from 'react-router-dom';
+import { AuthContext } from '../../contexts/AuthProvider/AuthProvider';
 
 const Header = () => {
+    const { user, logOut } = useContext(AuthContext);
+
+    const handleLogOut = () => {
+        logOut()
+            .then(() => { })
+            .catch(error => console.error(error))
+    }
     return (
         <div>
             <Navbar collapseOnSelect expand="lg" bg="light" variant="light">
@@ -20,10 +29,28 @@ const Header = () => {
 
                         </Nav>
                         <Nav className='mx-3 px-5 fs-5 fw-bolder'>
-                            <Link to='/'><Nav.Link href="#pricing">Courses</Nav.Link></Link>
-                            <Link to='/blog'><Nav.Link href="#pricing">Blog </Nav.Link></Link>
-                            <Link to='/login'><Nav.Link href="#pricing">Log In </Nav.Link></Link>
-                            <Nav.Link href="#pricing"><FontAwesomeIcon icon={faUser} /> User</Nav.Link>
+                            <Link to='/'><Nav.Link >Courses</Nav.Link></Link>
+                            <Link to='/blog'><Nav.Link>Blog </Nav.Link></Link>
+                            <>
+                                {
+                                    user?.uid ?
+                                        <>
+
+                                            <Nav.Link className='text-primary'>{user?.displayName}</Nav.Link>
+                                            <Nav.Link href="#pricing" onClick={handleLogOut}> <Button variant="dark">Log out</Button>  </Nav.Link>
+
+                                        </>
+                                        :
+                                        <>
+                                            <Link to='/login'><Nav.Link href="#pricing"><Button variant="dark">Log In</Button> </Nav.Link></Link>
+                                            <Nav.Link href="#pricing"><FontAwesomeIcon icon={faUser} /> User</Nav.Link>
+                                        </>
+                                }
+
+
+                            </>
+
+
                         </Nav>
                     </Navbar.Collapse>
                 </Container>
--- src/Pages/Login/Login.js
+++ src/Pages/Login/Login.js
@@ -1,33 +1,46 @@
-import React, { useContext } from 'react';
+import React, { useContext, useState } from 'react';
 import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
-import { useNavigate } from 'react-router-dom';
+import { useLocation, useNavigate, Link } from 'react-router-dom';
 import { AuthContext } from '../../contexts/AuthProvider/AuthProvider';
 
 
-
 const Login = () => {
-
-    const { signIn } = useContext(AuthContext);
+    const [error, setError] = useState('');
+    const { signIn, setLoading } = useContext(AuthContext);
     const navigate = useNavigate();
+    const location = useLocation();
+
+    const from = location.state?.from?.pathname || '/';
 
     const handleSubmit = event => {
         event.preventDefault();
         const form = event.target;
         const email = form.email.value;
         const password = form.password.value;
+
         signIn(email, password)
             .then(result => {
                 const user = result.user;
                 console.log(user);
                 form.reset();
-                navigate('/')
+                setError('');
+                if (user.emailVerified) {
+                    navigate(from, { replace: true });
+                }
+
             })
-            .catch(error => console.error(error))
+            .catch(error => {
+                console.error(error)
+                setError(error.message);
+            })
+            .finally(() => {
+                setLoading(false);
+            })
     }
 
     return (
-        <Form onSubmit={handleSubmit}>
+        <Form onSubmit={handleSubmit} className="m-5 p-5 w-50 ">
             <Form.Group className="mb-3" controlId="formBasicEmail">
                 <Form.Label>Email address</Form.Label>
                 <Form.Control name="email" type="email" placeholder="Enter email" required />
@@ -39,12 +52,19 @@ const Login = () => {
                 <Form.Control name="password" type="password" placeholder="Password" required />
             </Form.Group>
 
-            <Button variant="primary" type="submit">
+            <Button className='mx-3' variant="primary" type="submit">
                 Login
             </Button>
-            <Form.Text className="text-danger">
-
+            OR
+            <Form.Text className="mx-3" >
+                <Link to='/register'>Create a new account</Link>
             </Form.Text>
+            <div>
+                <Form.Text className="text-danger">
+                    {error}
+                </Form.Text>
+            </div>
+
         </Form>
     );
 };
